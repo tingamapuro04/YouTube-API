80c20bc2d7812a95af19b8f4f8589ec15e3ad18a
tingamapuro04 <alponcemobutu100@gmail.com>
2022-12-11 00:22:51 +0300

    Inside

--- app.js
+++ app.js
@@ -1,4 +1,28 @@
-const url = 'https://api.nytimes.com/svc/books/v3/lists/current/hardcover-fiction.json';
-const key = 'hBB9c023ujHsB9BiM4YgHVhYzLhIO2nF';
-const unordered = document.querySelector('.unordered');
+const displayer = (kk) => {
+  console.log(kk);
+};
 
+const calculator = (num1, num2, secondFunc) => {
+  const product = num1 * num2;
+  secondFunc(product);
+};
+
+const sayHello = () => {
+  let date = new Date();
+  const hour = date.getHours();
+  const minutes = date.getMinutes();
+  const seconds = date.getSeconds();
+  let time = `The time now is ${hour}:${minutes}:${seconds}`
+  if (hour >= 12 && hour <= 23) {
+    time = time.concat('PM');
+  } else {
+    time = time.concat('AM');
+  }
+  document.querySelector('body').textContent = time;
+};
+
+setInterval(sayHello, 1000);
+
+const myNumbers = [4, 1, -20, -7, 5, 9, -6, 10];
+const newL = myNumbers.filter((item) => item >= 0);
+console.log(myNumbers);
